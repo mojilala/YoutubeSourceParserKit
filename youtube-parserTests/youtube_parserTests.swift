a4e4c9b64bbf6aed5aeca98d8b048e3ace4e7337
Toygar Dündaralp <tdundaralp@gmail.com>
2015-07-12 15:11:57 -0700

    Update testcase file

--- youtube-parserTests/youtube_parserTests.swift
+++ youtube-parserTests/youtube_parserTests.swift
@@ -81,7 +81,20 @@ class youtube_parserTests: XCTestCase {
     }
   }
 
-  func testh264videosWithYoutubeURLBlock() {
+  func testVideoQuality(){
+    if let videoURL = NSURL(string: "http://www.youtube.com/watch?v=1hZ98an9wjo") {
+      Youtube.h264videosWithYoutubeURL(videoURL, completion: { (videoInfo, error) -> Void in
+        XCTAssertNotNil(videoInfo, "video dictionary is nil")
+        if let info = videoInfo as [String:AnyObject]? {
+          if let quality = info["quality"] as? String {
+            XCTAssertEqual(quality, "hd720", "quality not equal")
+          }
+        }
+      })
+    }
+  }
+
+  func testItagControl(){
     if let videoURL = NSURL(string: "http://www.youtube.com/watch?v=1hZ98an9wjo") {
       Youtube.h264videosWithYoutubeURL(videoURL, completion: { (videoInfo, error) -> Void in
         XCTAssertNotNil(videoInfo, "video dictionary is nil")
@@ -89,18 +102,39 @@ class youtube_parserTests: XCTestCase {
           if let itag = info["itag"] as? String {
             XCTAssertEqual(itag, "22", "itag not equal")
           }
-          if let quality = info["quality"] as? String {
-            XCTAssertEqual(quality, "hd720", "quality not equal")
-          }
-          if let fallback_host = info["fallback_host"] as? String {
-            XCTAssertEqual(fallback_host, "tc.v7.cache1.googlevideo.com", "fallback_host not equal")
-          }
+        }
+      })
+    }
+  }
+
+  func testVideoTypeControl(){
+    if let videoURL = NSURL(string: "http://www.youtube.com/watch?v=1hZ98an9wjo") {
+      Youtube.h264videosWithYoutubeURL(videoURL, completion: { (videoInfo, error) -> Void in
+        XCTAssertNotNil(videoInfo, "video dictionary is nil")
+        if let info = videoInfo as [String:AnyObject]? {
           if let type = info["type"] as? String {
             XCTAssertEqual(type, "video/mp4; codecs=\"avc1.64001F, mp4a.40.2\"", "type not equal")
           }
         }
       })
     }
+  }
+
+  func testFallbackHostControl(){
+    if let videoURL = NSURL(string: "http://www.youtube.com/watch?v=1hZ98an9wjo") {
+      Youtube.h264videosWithYoutubeURL(videoURL, completion: { (videoInfo, error) -> Void in
+        XCTAssertNotNil(videoInfo, "video dictionary is nil")
+        if let info = videoInfo as [String:AnyObject]? {
+          if let fallback_host = info["fallback_host"] as? String {
+            XCTAssertEqual(fallback_host, "tc.v7.cache1.googlevideo.com", "fallback_host not equal")
+          }
+        }
+      })
+    }
+  }
+
+
+  func testIsLiveVideoTest() {
     if let liveVideoURL = NSURL(string: "https://www.youtube.com/watch?v=rxGoGg7n77A"){
       Youtube.h264videosWithYoutubeURL(liveVideoURL, completion: { (videoInfo, error) -> Void in
         XCTAssertNotNil(videoInfo, "video dictionary is nil")
@@ -110,5 +144,4 @@ class youtube_parserTests: XCTestCase {
       })
     }
   }
-    
 }
