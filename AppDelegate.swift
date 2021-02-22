2a8b4e1c7c4c09348c5f5fea3c347c3146fa73cf
Nagib Bin Azad <nagib@revesoft.com>
2021-02-22 12:19:40 +0600

    project backup

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -13,12 +13,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    let testURL = NSURL(string: "https://www.youtube.com/watch?v=swZJwZeMesk")!
+    let testURL = NSURL(string: "https://www.youtube.com/watch?v=_SCdj9d8SUQ&list=RD_SCdj9d8SUQ&start_radio=1")!
     Youtube.h264videosWithYoutubeURL(youtubeURL: testURL) { (videoInfo, error) -> Void in
       if let videoURLString = videoInfo?["url"] as? String,
         let videoTitle = videoInfo?["title"] as? String {
           print("\(videoTitle)")
           print("\(videoURLString)")
+        URLSession.shared.dataTask(with: URL(string: videoURLString)!, completionHandler: { (data, response, error) in
+            if let data = data {
+                print(data.count)
+            }
+        }).resume()
       }
     }
     return true
